17459fc380f459af5fa4e1f93b3d95b63b6fe77a
= <zrsmead@gmail.com>
2017-10-29 18:04:48 -0700

    practicing with containers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title: 'Javascript: the Good Parts' },
+		{ title: 'Qwertymaster' },
+		{ title: 'The Best Cookbook in the Universe' },
+		{ title: 'Gooby pls' }
+	]
+}

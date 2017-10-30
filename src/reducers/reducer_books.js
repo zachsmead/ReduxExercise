c0879524cf99883fb78b295cae53ae889da218fe
= <zrsmead@gmail.com>
2017-10-29 21:54:46 -0700

    added conditional rendering to reducer flow

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
 	return [
-		{ title: 'Javascript: the Good Parts' },
-		{ title: 'Qwertymaster' },
-		{ title: 'The Best Cookbook in the Universe' },
-		{ title: 'Gooby pls' }
+		{ title: 'Javascript: the Good Parts', pages: 101 },
+		{ title: 'Qwertymaster', pages: 200 },
+		{ title: 'The Best Cookbook in the Universe', pages: 60 },
+		{ title: 'Gooby pls', pages: 42 }
 	]
 }

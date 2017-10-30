23f7f68f512f67ad5760c05e9165eb0f36ac9f0f
= <zrsmead@gmail.com>
2017-10-29 18:24:55 -0700

    containers work

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-export default class BookList extends Component {
+import { connect } from 'react-redux';
+
+export class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
@@ -18,3 +20,14 @@ export default class BookList extends Component {
 	}
 }
 
+
+function mapStateToProps(state) {
+	// Whatever is returned here will show up as props inside of BookList
+
+	// ALSO IMPORTANT: if the state ever changes, this container will instantly re-render with a new list of books.
+	return {
+		books: state.books
+	};
+}
+
+export default connect(mapStateToProps)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
 	books: BooksReducer

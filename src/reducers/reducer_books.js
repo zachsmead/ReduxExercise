e40e607d6457120a44d239ee861a11454cf22a93
= <zrsmead@gmail.com>
2017-12-22 03:18:23 -0800

    more docs

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,3 +1,8 @@
+// Stephen Grider prefers to prepend filenames with the type of file they are.
+// so with the reducers in this folder, they are prepended by 'reducer_' for
+// the sake of clarity. because there are going to end up being a lot of files
+// with the word 'book' in them.
+
 export default function () {
 	return [
 		{ title: 'Javascript: the Good Parts', pages: 101 },
